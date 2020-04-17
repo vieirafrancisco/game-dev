5f63811724d5d859f470233e80bfe0a421d69d1d
Francisco Neto <fvcneto.master98@gmail.com>
2020-04-17 17:54:11 -0300

    create a camera class to show players map vision

---
+++ pygame/pokemon/game/map/camera.py
@@ -0,0 +1,30 @@
+from settings import *
+
+class Camera:
+    def __init__(self, player, tmap):
+        self.player = player
+        self.tmap = tmap
+        self.dx, self.dy = 0, 0
+
+    def show(self, master_surface):
+        # corner pins
+        x0 = self.dx >> 5
+        x1 = (self.dx >> 5) + T_WIDTH
+        y0 = self.dy >> 5
+        y1 = (self.dy >> 5) + T_HEIGHT
+        print(x0, x1, y0, y1)
+        for j in range(y0, y1 + 1):
+            y = (j * TILE_SIZE - self.dy)
+            for i in range(x0, x1 + 1):
+                x = (i * TILE_SIZE - self.dx)
+                dest = (x, y)
+                if i >= 0 and j >= 0 and i < self.tmap.cols and j < self.tmap.rows:
+                    entity = self.tmap.get_entity(i, j)
+                    entity.start(self, self.tmap.surface, dest)
+                else:
+                    self.tmap.surface.blit(self.tmap.src_img, dest, self.tmap.tiles["default"])
+        master_surface.blit(self.tmap.surface, (0,0))
+        self.player.show(master_surface)
+
+    def update(self):
+        self.player.move(self)
